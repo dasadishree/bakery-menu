a6d11180af33bdebb33d27bdd069196b8d4b4ac5
Adishree Das <89429561+dasadishree@users.noreply.github.com>
2024-07-24 02:50:06 +0000

    added readme & prices

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# p5.js
-
-This is the Template Repl for p5.js.
-
-p5.js is a JavaScript library for creative coding, with a focus on making coding accessible and inclusive for artists, designers, educators, beginners, and anyone else! p5.js is free and open-source because we believe software, and the tools to learn it, should be accessible to everyone.
-
-[Check out the official docs here](https://p5js.org/reference/).
-
-
+Hi everyone!! This was originally supposed to be more of an interactive game - but then I changed it to be more like a cute little bakery's menu.
--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let scene = 1;
-let playButton, crossiantButton, donutButton, cupcakeButton;
+let playButton, crossiantButton, donutButton, cupcakeButton, pricesButton;
 let crossiantImg, donutImg, cupcakeImg;
+let matchaButton, hotCocoaButton, bobaButton, matchaImg, hotCocoaImg, bobaImg;
 function preload(){
   donutImg = loadImage('assets/donut.png')
   crossiantImg = loadImage('assets/crossiant.png')
   cupcakeImg = loadImage('assets/cupcake.png')
+  matchaImg = loadImage('assets/matcha.png')
+  hotCocoaImg = loadImage('assets/hotcocoa.webp')
+  bobaImg = loadImage('assets/boba.png')
 }
 
 function setup() {
@@ -15,17 +19,33 @@ function setup() {
   stroke(10);
   playButton = new Sprite(200, 288, 100, 50, "k");
   playButton.color = color("white");
-  playButton.text = "PLAY";
+  playButton.text = "MENU";
   playButton.textSize = 30;
+
+  //play button
+  stroke(10);
+  pricesButton = new Sprite(-200, -288, 110, 50, "k");
+  pricesButton.color = color("white");
+  pricesButton.text = "PRICES";
+  pricesButton.textSize = 30;
 
   crossiantButton = new Sprite(crossiantImg, -200,-50,1, 1);
   crossiantImg.resize(125, 125);
 
   donutButton = new Sprite(donutImg, -200,-50,1, 1);
   donutImg.resize(125, 125);
+  
   cupcakeButton = new Sprite(cupcakeImg, -200,-50,1, 1);
   cupcakeImg.resize(175, 125);
 
+  bobaButton = new Sprite(bobaImg, -200,-50,1, 1);
+  bobaImg.resize(125, 125);
+  
+  matchaButton = new Sprite(matchaImg, -200,-50,1, 1);
+  matchaImg.resize(175, 125);
+
+  hotCocoaButton = new Sprite(hotCocoaImg, -200,-50,1, 1);
+  hotCocoaImg.resize(175, 125);
 }
 
 function draw() {
@@ -70,14 +90,43 @@ function draw() {
   if(scene===2) {
       background("pink");
       playButton.pos = {x:-500, y:-500}
+      pricesButton.pos = {x:315, y:100}
       fill("hotpink");
       textSize(20);
       text("hi everyone!! welcome to the cozy\n cafe, here's our menu!",50, 50);
-      fill("magenta");
-      textSize(15);
-      crossiantButton.pos = {x:75, y:200}
-      donutButton.pos = {x:210, y:200}
-      cupcakeButton.pos = {x:350, y:200}
+      crossiantButton.pos = {x:75, y:180}
+      donutButton.pos = {x:210, y:180}
+      cupcakeButton.pos = {x:350, y:180}
+      bobaButton.pos = {x:350, y:325}
+      matchaButton.pos = {x:210, y:325}
+      hotCocoaButton.pos = {x:75, y:325}
+    
+    //button
+    if (pricesButton.mouse.presses()) {
+      scene = 3;
+    }
   }
-  
+  if(scene==3) {
+    background("hotpink");
+    fill("pink");
+    textSize(20);
+    text("prices!! dw, everything is on the house \n for our favorite customers (you!!!)",50, 50);
+    crossiantButton.pos = {x:-575, y:170}
+    donutButton.pos = {x:-210, y:170}
+    cupcakeButton.pos = {x:-350, y:170}
+    bobaButton.pos = {x:-350, y:325}
+    matchaButton.pos = {x:-210, y:325}
+    hotCocoaButton.pos = {x:-575, y:325}
+    pricesButton.pos = {x:-315, y:100}
+
+    fill(255);
+    text("CROSSIANT:\n $4", 20, 160);
+    text("DONUT:\n $2", 160, 160);
+    text("CUPCAKE:\n $5", 290, 160);
+    text("BOBA:\n $9", 20, 300);
+    text("MATCHA:\n $6", 150, 300);
+    text("HOT COCOA:\n $6", 270, 300);
+
+
+  }
 }
